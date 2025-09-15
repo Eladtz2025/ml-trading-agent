0fd1828ce6e6d99c355accc745d6c0923762683f
Eladtz2025 <eladtz@gmail.com>
2025-09-15 18:14:15 +0300

    Add files via upload

---
+++ features/macd.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def macd(df: pd.DataFrame, fast=12, slow=26, signal=9):
+    ema_fast = df['close'].ewm(span=fast, adjust=False).mean()
+    ema_slow = df['close'].ewm(span=slow, adjust=False).mean()
+    macd_line = ema_fast - ema_slow
+    signal_line = macd_line.ewm(span=signal, adjust=False).mean()
+    histogram = macd_line - signal_line
+    return pd.DataFrame({'macd': macd_line, 'signal': signal_line, 'hist': histogram})
