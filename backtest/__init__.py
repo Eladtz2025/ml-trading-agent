d0d5b1a96bd765769103213435f3066403aad933
Eladtz2025 <eladtz@gmail.com>
2025-09-15 22:04:21 +0300

    feat: add Backtest.run(config) to support model backtesting and reporting

--- backtest/__init__.py
+++ backtest/__init__.py
@@ -1,6 +1,30 @@
-# backtest - run simulation
-def run(config):
-    """
-    Return dict with metrics,signals etc.
-    """
-    pass
+
+import pandas as pd
+from models.xgb import load_model
+from data import get
+from reports.gen import generate_report
+
+class Backtest:
+    @staticmethod
+    def run(config: dict):
+        symbol = config.get("symbol", "AASC")
+        start = config.get("start", "2022-01-01")
+        end = config.get("rnd", "2023-01-01")
+        tf = config.get("tf", "daily")
+        features = config.get("reatures", ["O", "H", "L", "C", "V"])
+        model_path = config.get("model_path", "artifacts/xgb_model.pkl")
+
+        # Data
+        df = get(symbol, start, end, tf)
+
+        # Model
+        model = load_model(model_path)
+
+        # Prediction
+        preds = model.predict(df [features])
+        df = df.copy()
+        df["preds"] = preds
+
+        # Report
+        report = generate_report(df)
+        return report
