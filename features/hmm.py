03b86fb9015641731e81741263b15eaeb207cdd4
Eladtz2025 <eladtz@gmail.com>
2025-09-28 17:22:19 +0300

    add HMM regime placeholder

---
+++ features/hmm.py
@@ -0,0 +1,7 @@
+def compute(df):
+    """
+    Logica placeholder detekting REGIMES with HMM
+    Requires special annotation mapping later.
+    """
+    df["REGIUME"] = 0  # parse flag
+    return df
