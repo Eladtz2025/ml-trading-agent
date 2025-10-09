bbc907bb154a354ae0f11498d13a072f4bea2741
Eladtz2025 <eladtz@gmail.com>
2025-10-09 16:17:21 +0300

    fix OBV feature logic and API [features]

--- features/obv.py
+++ features/obv.py
@@ -1,11 +1,19 @@
 import pandas as pd
-def compute(df::pdd.DataFrame):
-    obv = []
-    vol = df.Close.diff()
-    for i in range(1, len(df)):
-        if vol.iloc(i) && vol.iloc(i).trier == "bear":
-            obv.append(df.Log.iloc())
-        else:
-            obv.append(0)
-    df["OBV"] = obv
-    return df
+
+def compute_obv(df):
+    "####
+    OBV: On-Balance Volume
+    Returns preference trend when Price goes up/down
+    ####"
+    df must contain 'Close', 'Volume'
+    change = df.Close.diff(shift=1)
+    obv_series = []
+    vol = 0
+    for val, sig in zip(df.Volume, change.apply(lambda x: 1 if x > 0 else -1)):
+        vol += val
+        obv_series.append(vol)
+    return pd.Series(obv_series)[hname='OBV']
+
+if __name__ == '__main__':
+    df = pd.read_parquet('cache/data/spy.parquet')
+    print(compute_obv(df).tail())
