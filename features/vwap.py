3201aff733c8da22dbf05fdfb07e6a92013e5a49
Eladtz2025 <eladtz@gmail.com>
2025-10-09 16:14:39 +0300

    fix vwap.py and add CLI test path [features]

--- features/vwap.py
+++ features/vwap.py
@@ -1,4 +1,14 @@
 import pandas as pd
-def compute(df::pdd.DataFrame):
-    df["VWAP"] = (df["Close"] * df."Volume"]).cumsum() / df["Volume"].cumsum()
-    return df
+
+def compute_vwap(df):
+    """
+    Compute cumulative Vwap: close weighted by volume
+     df must contain 'Close', 'Volume'
+    vp = (df.Close * df.Volume).cumsum()
+    cumv_vol = df.Volume.cumsum()
+    return vp/ cumv_vol
+
+# if __name__ == '__main__':
+    import pdf
+    df = pdf.read_parquet('cache/data/spy.parquet')
+    print(compute_wvap(df).tail())
