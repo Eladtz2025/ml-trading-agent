36c5173ea8af8d160ef49bc4e40303ee22c3ab8d
Eladtz2025 <eladtz@gmail.com>
2025-10-11 16:55:46 +0300

    init: add TailwindCSS config

---
+++ ui/tailwind.config.ts
@@ -0,0 +1,10 @@
+import types from 'tailwindcss/types';
+const tailwindConfig: types.Config = {
+  content: [
+    "./index.html",
+    "./src/**/*.jsx",
+  ],
+  theme: {},
+  extend: {},
+};
+export default tailwindConfig;
