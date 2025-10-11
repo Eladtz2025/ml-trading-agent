4aa0bd796bbab55351f0a39c544d01a235d4d964
Eladtz2025 <eladtz@gmail.com>
2025-10-11 17:04:01 +0300

    update: App component to load Dashboard with padding

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
-export default function App() {
-  return (
-    <div className="min-container">
-      <Dashboard />
-    </div>
-  );
+function App() {
+  return <div className="min-app p-4"><Dashboard/></div>;
 }
+export default App;
