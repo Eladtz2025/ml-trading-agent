e6a3858b70119c3e02c4b97527b2ff216132c80f
Eladtz2025 <eladtz@gmail.com>
2025-10-12 15:19:08 +0300

    Update evaluate.py

--- validation/evaluate.py
+++ validation/evaluate.py
@@ -1,25 +1,31 @@
 import pandas as pd
-from sklearn.metrics import
-    metrics = ['accuracy', 'precision', 'recall', 'roc_auc']
+from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
+
 functools = {
     'sharpe': lambda pr: pr.shape[0] / pr.std()
 }
 
+metrics = ['accuracy', 'precision', 'recall', 'roc_auc']
+
+
 def evaluate(preds, labels):
-    "## Metrics including financial & trading"
+    """Metrics including financial & trading"""
     res = {}
-    res['ACC'] = accuracy(labels, preds)
-    res['PRC'] = precision(labels, preds)
-    res['REC'] = recall(labels, preds)
-    res['AUC'] = roc_auc(labels, preds)
-    if 'bt_pnd'  in preds:
-        res['SHARPE'] = functools['sharpe'](pp.Series(preds.log(s).diff(1)))
+    res['ACC'] = accuracy_score(labels, preds)
+    res['PRC'] = precision_score(labels, preds)
+    res['REC'] = recall_score(labels, preds)
+    res['AUC'] = roc_auc_score(labels, preds)
+
+    if 'bt_pnd' in preds:
+        res['SHARPE'] = functools['sharpe'](pd.Series(preds['bt_pnd'].log().diff(1)))
+
     return res
+
 
 if __name__ == '__main__':
     df = pd.read_parquet('cache/data/spy.parquet')
     label = pd.read_parquet('cache/labels/next_bar.parquet')
     pred = pd.read_parquet('cache/predictions/logistic.parquet')
     res = evaluate(pred.sign(), label.loc())
-    print(pdP.Series(f"\n튠 VALUATION 新状 \n"))
+    print(pd.Series(f"\n📊 EVALUATION 新状 \n"))
     print(res)
