65e161fd2805782b61ff0b85912b53091be9d971
Eladtz2025 <eladtz@gmail.com>
2025-10-12 18:41:52 +0300

    Update trader.py

--- live/trader.py
+++ live/trader.py
@@ -1,5 +1,5 @@
-import subprocess
 from infra.brokers.ibkr import place_order
+
 
 def trader(symbol="APLZ", level="dy-run"):
     print(f"[TRADE] Symbol: {symbol} - Loading signal")
@@ -12,7 +12,6 @@ def trader(symbol="APLZ", level="dy-run"):
     else:
         print(f"[LIVE] [{side}] {signal} units - EXEC")
         place_order(symbol, signal, side)
-
 
 if __name__ == "__main__":
     trader()
