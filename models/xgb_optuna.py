d1b9d481e91d7b2eb56ad98c06a176405ac8e6b4
Eladtz2025 <eladtz@gmail.com>
2025-10-12 18:42:29 +0300

    Update xgb_optuna.py

--- models/xgb_optuna.py
+++ models/xgb_optuna.py
@@ -1,6 +1,6 @@
 import xgboost as xgb
 import optuna
-import pandas as pd
+
 def train_optuna(X, y, n_trials=20):
     def objective(trial):
         params = {
@@ -10,7 +10,7 @@ def train_optuna(X, y, n_trials=20):
             "max_depth": trial.suggest_int("max_depth", 3, 8),
             "eta": trial.suggest_float("eta", 0.01, 0.3),
             "subsample": trial.suggest_float("subsample", 0.5, 1.0),
-            "colsample_bytree": trial.suggest_float("colsample_bytree", 0.5, 1.0)
+            "colsample_bytree": trial.suggest_float("colsample_bytree", 0.5, 1.0),
         }
         dtrain = xgb.DMatrix(X, label=y)
         cv = xgb.cv(params, dtrain, num_boost_round=100, nfold=5, seed=42, early_stopping_rounds=10)
