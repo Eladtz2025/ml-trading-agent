e7514330955ce970f2d87b4fb11d86ce6463f1bf
Eladtz2025 <eladtz@gmail.com>
2025-10-12 22:58:24 +0300

    Update vite.config.ts

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
+    emptyOutDir: true
   },
   server: {
     host: '0.0.0.0',
-    port: 4173,
-  },
-});
+    port: 4173
+  }
+})
