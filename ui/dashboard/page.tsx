a5eaa0d26eb556be3e43bcb45d984e85ef724232
Eladtz2025 <eladtz@gmail.com>
2025-10-16 17:44:15 +0300

    feat(ui): add PerformancePanel to dashboard

--- ui/dashboard/page.tsx
+++ ui/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import AgentChat from "./components/AgentChat";
 import DecisionList from "./components/DecisionList";
 import ExplanationPanel from "./components/ExplanationPanel";
+import PerformancePanel from "./components/PerformancePanel";
 import { classNames } from "clsx";
 
 export default function DashboardPage() {
@@ -10,8 +11,9 @@ export default function DashboardPage() {
         <h2 className="text-lg">Phoenix Agent Chat</h2>
         <AgentChat />
       </div>
-      <DecisionList />
       <ExplanationPanel />
+      <PerformancePanel />
+      <DecisionList />
     </div>
   );
 }
