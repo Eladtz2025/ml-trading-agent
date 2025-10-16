fb94413662348dcfa93d32d5078c0f7859fab199
Eladtz2025 <eladtz@gmail.com>
2025-10-16 17:17:29 +0300

    feat(ui): add ExplanationPanel under tab

--- ui/dashboard/page.tsx
+++ ui/dashboard/page.tsx
@@ -1,15 +1,17 @@
 import AgentChat from "./components/AgentChat";
 import DecisionList from "./components/DecisionList";
+import ExplanationPanel from "./components/ExplanationPanel";
 import { classNames } from "clsx";
 
 export default function DashboardPage() {
   return (
     <div className="flex justify-center">
-      <div className="space-y2-pn-2 border right">
+      <div className="space-y-2-pn-2 border right">
         <h2 className="text-lg">Phoenix Agent Chat</h2>
         <AgentChat />
       </div>
       <DecisionList />
+      <ExplanationPanel />
     </div>
   );
 }
